d6fb4ba9ad6ac867e8720f62edea7fdfac77fc8d
Jamie Dixon <jamie@jamie-dixon.co.uk>
2017-08-11 16:13:09 +0100

    Mods for V3
    Modify prod build
    
    Move package over to @connected-home
    
    Downgrade jest and babel-jest to support V3
    
    Undo commented template section. Should work now that were referencing react-scripts as part of CRA rather than a folder in our project
    
    Delete yarn.lock file and use npm for this proj instead
    
    Remove fs package that was added by us for some reason
    
    Add HivehomeWebappFaviconsWebpackPlugin with proper config
    
    Remove path package from devDependencies

--- packages/react-scripts/scripts/start.js
+++ packages/react-scripts/scripts/start.js
@@ -13,6 +13,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
+process.env.DIST_ENV = process.env.DIST_ENV ? process.env.DIST_ENV : 'local-dev';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
